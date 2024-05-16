ba25bea32c880266b0f31f84956d8ca089899c0d
Lino2109 <167836862+Lino2109@users.noreply.github.com>
2024-05-16 12:45:24 -0400

    Update page.js

--- page.js
+++ page.js
@@ -1,51 +1,58 @@
-module.exports = {
-    // Inputs
-    fromField: '#from',
-    toField: '#to',
-    phoneNumberField: '#phone',
-    codeField: '#code',
-    // Buttons
-    callATaxiButton: 'button=Call a taxi',
-    phoneNumberButton: '//div[starts-with(text(), "Phone number")]',
-    nextButton: 'button=Next',
-    confirmButton: 'button=Confirm',
-    // Modals
-    phoneNumberModal: '.modal',
-    // Functions
-    fillAddresses: async function(from, to) {
-        const fromField = await $(this.fromField);
-        await fromField.setValue(from);
-        const toField = await $(this.toField);
-        await toField.setValue(to);
-        const callATaxiButton = await $(this.callATaxiButton);
-        await callATaxiButton.waitForDisplayed();
-        await callATaxiButton.click();
+const page = {
+    fillAddresses: async (pickup, dropoff) => {
+        const pickupInput = await $('//input[@name="pickup"]');
+        await pickupInput.setValue(pickup);
+        const dropoffInput = await $('//input[@name="dropoff"]');
+        await dropoffInput.setValue(dropoff);
     },
-    fillPhoneNumber: async function(phoneNumber) {
-        const phoneNumberButton = await $(this.phoneNumberButton);
-        await phoneNumberButton.waitForDisplayed();
-        await phoneNumberButton.click();
-        const phoneNumberModal = await $(this.phoneNumberModal);
-        await phoneNumberModal.waitForDisplayed()
-        const phoneNumberField = await $(this.phoneNumberField);
-        await phoneNumberField.waitForDisplayed();
-        await phoneNumberField.setValue(phoneNumber);
+    phoneNumberButtonXPath: '//button[@id="phoneNumberButton"]',
+    phoneNumberModalXPath: '//div[@id="phoneNumberModal"]',
+    submitPhoneNumber: async (phoneNumber) => {
+        const phoneInput = await $('//input[@name="phone"]');
+        await phoneInput.setValue(phoneNumber);
+        const submitButton = await $('//button[@id="submitPhoneNumber"]');
+        await submitButton.click();
     },
-    submitPhoneNumber: async function(phoneNumber) {
-        await this.fillPhoneNumber(phoneNumber);
-        // we are starting interception of request from the moment of method call
-        await browser.setupInterceptor();
-        await $(this.nextButton).click();
-        // we should wait for response
-        // eslint-disable-next-line wdio/no-pause
-        await browser.pause(2000);
-        const codeField = await $(this.codeField);
-        // collect all responses
-        const requests = await browser.getRequests();
-        // use first response
-        await expect(requests.length).toBe(1)
-        const code = await requests[0].response.body.code
-        await codeField.setValue(code)
-        await $(this.confirmButton).click()
+    selectPlan: async (plan) => {
+        const planButton = await $(`//button[text()="${plan}"]`);
+        await planButton.click();
     },
+    openCreditCardModal: async () => {
+        const addCardButton = await $('//button[@id="addCreditCard"]');
+        await addCardButton.click();
+    },
+    fillCreditCardDetails: async (number, expiry, cvv) => {
+        const numberInput = await $('//input[@id="cardNumber"]');
+        await numberInput.setValue(number);
+        const expiryInput = await $('//input[@id="expiryDate"]');
+        await expiryInput.setValue(expiry);
+        const cvvInput = await $('//input[@id="code"]');
+        await cvvInput.setValue(cvv);
+        await cvvInput.keys('Tab'); // Simulate TAB key press
+    },
+    clickAway: async () => {
+        const body = await $('//body');
+        await body.click();
+    },
+    writeMessageForDriver: async (message) => {
+        const messageInput = await $('//textarea[@id="driverMessage"]');
+        await messageInput.setValue(message);
+    },
+    orderBlanketAndHandkerchiefs: async () => {
+        const orderButton = await $('//button[@id="orderBlanket"]');
+        await orderButton.click();
+    },
+    orderIceCreams: async (quantity) => {
+        const iceCreamInput = await $('//input[@id="iceCreamQuantity"]');
+        await iceCreamInput.setValue(quantity);
+        const orderButton = await $('//button[@id="orderIceCream"]');
+        await orderButton.click();
+    },
+    waitForCarSearchModal: async () => {
+        const carSearchModal = await $('//div[@id="carSearchModal"]');
+        await carSearchModal.waitForDisplayed();
+    }
 };
+
+module.exports = page;
+;
