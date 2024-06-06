dede9193951a75450dc57567053e1c9d067fe0a5
Lino2109 <167836862+Lino2109@users.noreply.github.com>
2024-06-06 12:16:03 -0400

    Update createAnOrder.e2e.js

--- test/specs/createAnOrder.e2e.js
+++ test/specs/createAnOrder.e2e.js
@@ -10,7 +10,7 @@ describe('Create an order', () => {
         await phoneNumberButton.click();
         const pnoneNumberModal = await $(page.phoneNumberModal);
         await expect(pnoneNumberModal).toBeExisting();
-    })
+    });
 
     it('should save the phone', async () => {
         await browser.url(`/`)
@@ -19,7 +19,7 @@ describe('Create an order', () => {
         await page.submitPhoneNumber(phoneNumber);
         const phoneNumberElement = await helper.getElementByText(phoneNumber);
         await expect(phoneNumberElement).toBeExisting();
-    })
+    });
 
     it('should select supportive plan', async () => {
         await browser.url('/')
@@ -27,8 +27,8 @@ describe('Create an order', () => {
         const supportivePlanButton = await $(page.supportivePlanButton);
         await supportivePlanButton.waitForDisplayed();
         await supportivePlanButton.click();
-      //  await expect(supportivePlanButton).toHaveAttributeContaining('class', 'selected');
-    })
+        await expect(supportivePlanButton).toHaveAttributeContaining('class', 'selected');
+    });
 
      it('should fill phone number and submit', async () => {
          await browser.url('/')
@@ -37,21 +37,23 @@ describe('Create an order', () => {
          await phoneNumberButton.waitForDisplayed();
          await phoneNumberButton.click();
          const phoneNumber = helper.getPhoneNumber("+1");
-//        await page.submitPhoneNumber(phoneNumber);
-     })
+         await page.submitPhoneNumber(phoneNumber);
+     });
 
      it('should add new card', async () => {
         await browser.url('/');
         await page.fillAddresses('East 2nd Street, 601', '1300 1st St');
         await page.fillCardDetails("123456789101","14");
-     })
+        await expect(page.fillCardDetails).toAddCard();
+    });
      
 
      it('should enter card details and CVV', async () => {
          await browser.url('/')
          await page.fillAddresses('East 2nd Street, 601', '1300 1st St');
-         await page.fillCardDetails("123456789101","14");
-     })
+         await page.fillCardDetails("123456789101","14"); 
+         await expect(page.fillCardDetails).this.fillCardDetails("123456789101","14");
+    });
     
     
      it('should enter message for driver', async () => {
@@ -63,19 +65,19 @@ describe('Create an order', () => {
         await expect(enterMessageForDriver).toHaveValue(message);
     });
     
-   it('should order blanket and handkerchief', async () => {
-    await browser.url('/');
-    await page.fillAddresses('East 2nd Street, 601', '1300 1st St');
-    await page.blanketHandkerchiefSelect();
-    await expect($(page.orderBlanketAndHanderchiefstatus)).toBeChecked();
-});
+     it('should order blanket and handkerchief', async () => {
+     await browser.url('/');
+     await page.fillAddresses('East 2nd Street, 601', '1300 1st St');
+     await page.blanketHandkerchiefSelect();
+     await expect($(page.orderBlanketAndHanderchiefstatus)).toBeChecked();
+    });
 
      it('should order 2 ice creams', async () => {
          await browser.url('/')
          await page.fillAddresses('East 2nd Street, 601', '1300 1st St');
          await page.iceCreamSelect();
         expect($(page.iceCreamValue)).toHaveValue(2);
- });
+    });
 
      it('should display car search modal', async () => {
          await browser.url('/')
@@ -87,8 +89,8 @@ describe('Create an order', () => {
          await $(page.orderButton).click();
          const carSearchModal = await $(page.carSearchModal);
          await carSearchModal.waitForDisplayed();
-//         expect($(page.carSearchModal)).toBeDisplayed();
-     });
+         expect($(page.carSearchModal)).toBeDisplayed();
+    });
 
-})
+});
 
